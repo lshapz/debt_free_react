1f18ad1405ab69c1e1fd0905025b83b85e552648
lshapz <laurashapiro01@gmail.com>
2016-12-14 11:14:55 -0500

    period clear fix

--- src/ducks/newcard.js
+++ src/ducks/newcard.js
@@ -15,6 +15,7 @@ export function createCard(formData){
       dispatch(setCard(response.card))
       dispatch(addNewCardtoUser(response.card))
       dispatch(foundCard())
+      dispatch(overWritePeriods(''))
       browserHistory.push('/user')
     }).catch((response)=>{
       let errors = response.responseJSON.error.join(', ')
